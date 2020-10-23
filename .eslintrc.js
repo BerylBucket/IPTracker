59b3f0a1589037ac32545a08d0470f54d1b20223
BerylBucket <71046403+BerylBucket@users.noreply.github.com>
2020-10-23 09:32:34 -0700

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: false,
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: "module",
+  },
+};
