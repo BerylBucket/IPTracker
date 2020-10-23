1cd006c94da9eb227826aa63783ef316acab20b4
BerylBucket <71046403+BerylBucket@users.noreply.github.com>
2020-10-23 09:30:22 -0700

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  publicPath: "/ArchStudio/",
+  pages: {
+    index: {
+      // entry for the page
+      entry: "src/main.js",
+      // the source template
+      template: "public/index.html",
+      // output as dist/index.html
+      filename: "index.html",
+      title: "Home",
+    }
+  }
+}
